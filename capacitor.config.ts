9e12bd4e6ce78270ea67ea7072b88a134fa87eef
Akhil Ennu <akhil.ennu@github.com>
2025-05-18 07:25:20 +0530

    Added Capacitor to convert to apk

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.gamehub',
+  appName: 'Game Hub',
+  webDir: 'dist'
+};
+
+export default config;
